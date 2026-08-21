092f0affde73c38de175af0b99349f4f0acd4407
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-08-20 21:50:15 -0500

    Remove unnecessary configurations for player, fixed by midi file loading

--- apps/player.py
+++ apps/player.py
@@ -26,10 +26,6 @@ import hardware
 import menu
 import os
 
-# Larger buffer needed to prevent stutters in audio when reading from SD
-hardware.BUFFER_SIZE = 32768 if hardware.is_rp2350() else 8192
-hardware.TASK_SLEEP = 0.1 #?
-
 hardware.init()
 
 DIR = "/sd/songs"
